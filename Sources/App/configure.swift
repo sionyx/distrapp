e352061fed06e5f240aff29634547034b2e01bc8
Vadim Balashov <balashov@corp.mail.ru>
2020-04-28 11:55:43 +0300

    Log env parameters

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,11 +7,21 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    let config = MySQLConfiguration(hostname: Environment.get("DATABASE_HOST")!,
+    let hostname = Environment.get("DATABASE_HOST")!
+    let username = Environment.get("DATABASE_USERNAME")!
+    let password = Environment.get("DATABASE_PASSWORD")!
+    let database = Environment.get("DATABASE_NAME")!
+
+    print("hostname: \(hostname)")
+    print("username: \(username)")
+    print("password: \(password.prefix(3))***\(password.suffix(3))")
+    print("database: \(database)")
+
+    let config = MySQLConfiguration(hostname: hostname,
                                     port: 3306,
-                                    username: Environment.get("DATABASE_USERNAME")!,
-                                    password: Environment.get("DATABASE_PASSWORD")!,
-                                    database: Environment.get("DATABASE_NAME")!,
+                                    username: username,
+                                    password: password,
+                                    database: database,
                                     tlsConfiguration: .forClient(certificateVerification: .none))
 
     app.databases.use(.mysql(configuration: config, maxConnectionsPerEventLoop: 8), as: .mysql)
