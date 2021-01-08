8ca97a2f51996ebbe30b59e0e9cb3b33641c1852
Vadim Balashov <balashov@corp.mail.ru>
2021-01-08 19:30:58 +0300

    Bot Listner rewritten to scheduleRepeatedAsyncTask

--- Sources/App/Tools/MyTeam/MyTeam.swift
+++ Sources/App/Tools/MyTeam/MyTeam.swift
@@ -7,8 +7,7 @@
 
 import Vapor
 import Queues
-
-
+import NIO
 
 enum MyTeam {
     class Configuration {
@@ -42,15 +41,18 @@ enum MyTeam {
         }
     }
 
-    class Listner {
+    class Listner: LifecycleHandler {
         private weak var app: Application!
         private var handlers: [MyTeamHandler]!
+        private var eventLoop: EventLoop!
+        private var task: RepeatedTask?
+        private var lastEventId = 0
 
         fileprivate init(app: Application) {
             self.app = app
+            app.lifecycle.use(self)
+            eventLoop = app.eventLoopGroup.next()
         }
-
-        var lastEventId = 0
 
         func configure(with handlers: [MyTeamHandler]) {
             let bot = MyTeam.Sender(client: app.client)
@@ -58,65 +60,65 @@ enum MyTeam {
             self.handlers.forEach { $0.configure(bot: bot) }
         }
 
+        func shutdown(_ application: Application) {
+            task?.cancel()
+        }
+
         func listen() {
-            let eventLoop = app.eventLoopGroup.next()
-            
-            _ = app.threadPool.runIfActive(eventLoop: eventLoop) { [weak app] () -> EventLoopFuture<Void> in
-                while true {
-                    guard let app = app else {
-                        break
-                    }
-                    do {
-                        app.logger.info("MyTeam start request")
-
-                        try app.client.post(URI(string: "\(MyTeam.Configuration.host)/bot/v1/events/get")) { [weak self] req in
-                            guard let self = self else {
-                                return
-                            }
-                            let message = ["token": MyTeam.Configuration.token,
-                                           "lastEventId": String(self.lastEventId),
-                                           "pollTime": String(MyTeam.Configuration.pollTime)]
-                            try req.content.encode(message, as: HTTPMediaType.formData)
-                        }.flatMap { [weak self] res -> EventLoopFuture<Void> in
-                            guard let self = self,
-                                  let response = try? res.content.decode(MyTeam.EventsResponse.self) else {
-                                return eventLoop.makeSucceededFuture(())
-                            }
-
-                            guard response.ok,
-                                  let events = response.events?.unpack() else {
-                                return eventLoop.makeSucceededFuture(())
-                            }
-
-                            let messages = events.compactMap { event -> MyTeam.MessagePayload? in
-                                guard event.type == .newMessage else {
-                                    return nil
-                                }
-
-                                return event.payload
-                            }
-
-                            let handles = messages.compactMap { message -> EventLoopFuture<Void>? in
-                                guard let handler = self.handlers.first(where: {$0.canHandle(message) }) else {
-                                    return nil
-                                }
-
-                                return handler.handle(message, eventLoop: eventLoop, db: app.db)
-                            }
-
-                            if let id = events.last?.eventId {
-                                self.lastEventId = id
-                            }
-
-                            return eventLoop.flatten(handles)
-                        }
-                        .wait()
-                    }
-                    catch {
-                        app.logger.report(error: error)
-                    }
+            task = eventLoop.scheduleRepeatedAsyncTask(initialDelay: .zero, delay: .zero) { [weak self] _ -> EventLoopFuture<Void> in
+                guard let self = self else {
+                    fatalError("MyTeam bot listen error")
                 }
-                return eventLoop.makeSucceededFuture(())
+                self.app.logger.info("MyTeam start request")
+                return self.botRequest()
+            }
+        }
+
+        private func botRequest() -> EventLoopFuture<Void> {
+            guard let eventLoop = self.eventLoop,
+                  let db = self.app?.db else {
+                fatalError("MyTeam bot request has no event loop")
+            }
+            return app.client.post(URI(string: "\(MyTeam.Configuration.host)/bot/v1/events/get")) { [weak self] req in
+                guard let self = self else {
+                    return
+                }
+                let message = ["token": MyTeam.Configuration.token,
+                               "lastEventId": String(self.lastEventId),
+                               "pollTime": String(MyTeam.Configuration.pollTime)]
+                try req.content.encode(message, as: HTTPMediaType.formData)
+            }.flatMap { [weak self] res -> EventLoopFuture<Void> in
+                guard let self = self,
+                      let response = try? res.content.decode(MyTeam.EventsResponse.self) else {
+                    return eventLoop.makeSucceededFuture(())
+                }
+
+                guard response.ok,
+                      let events = response.events?.unpack() else {
+                    return eventLoop.makeSucceededFuture(())
+                }
+
+                let messages = events.compactMap { event -> MyTeam.MessagePayload? in
+                    guard event.type == .newMessage else {
+                        return nil
+                    }
+
+                    return event.payload
+                }
+
+                let handles = messages.compactMap { message -> EventLoopFuture<Void>? in
+                    guard let handler = self.handlers.first(where: {$0.canHandle(message) }) else {
+                        return nil
+                    }
+
+                    return handler.handle(message, eventLoop: eventLoop, db: db)
+                }
+
+                if let id = events.last?.eventId {
+                    self.lastEventId = id
+                }
+
+                return eventLoop.flatten(handles)
             }
         }
     }
